92960cf76ce584a9a5713841c1c921c94ba9bf8f
imufiid <imufiid98@gmail.com>
2023-12-17 18:57:19 +0700

    feat: Setup app with navigation

---
+++ src/screens/UserScreen.tsx
@@ -0,0 +1,23 @@
+import {View, Text, StyleSheet} from 'react-native';
+
+export const UserScreen = () => {
+  return (
+    <View style={styles.rootContainer}>
+      <Text>
+        This is the <Text style={styles.highlight}>"User"</Text> screen!
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  highlight: {
+    fontWeight: 'bold',
+    color: '#eb1064',
+  },
+});
