084273fe5d99f34123312fac60a4ae68f8cc328f
imufiid <imufiid98@gmail.com>
2023-12-19 06:33:01 +0700

    feat: Configuration drawer

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import {NavigationContainer} from "@react-navigation/native";
 import {createDrawerNavigator} from "@react-navigation/drawer";
 import {WelcomeScreen} from "./src/screens/WelcomeScreen";
 import {UserScreen} from "./src/screens/UserScreen";
+import {Ionicons} from '@expo/vector-icons';
 import 'react-native-gesture-handler';
 
 const Drawer = createDrawerNavigator()
@@ -9,12 +10,32 @@ const Drawer = createDrawerNavigator()
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="User">
+      <Drawer.Navigator
+        initialRouteName="Welcome"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#3c0a6b'
+          },
+          headerTintColor: 'white',
+          drawerActiveBackgroundColor: '#F0E1FF',
+          drawerActiveTintColor: '#3c0a6b',
+        }}>
         <Drawer.Screen
           name="Welcome"
-          component={WelcomeScreen}/>
+          component={WelcomeScreen}
+          options={{
+            drawerLabel: 'Welcome Screen',
+            drawerIcon: ({color, size}) => {
+              return <Ionicons name="home" color={color} size={size}/>
+            }
+          }}/>
         <Drawer.Screen
           name="User"
+          options={{
+            drawerIcon: ({color, size}) => {
+              return <Ionicons name="person" color={color} size={size}/>
+            }
+          }}
           component={UserScreen}/>
       </Drawer.Navigator>
     </NavigationContainer>
--- src/screens/UserScreen.tsx
+++ src/screens/UserScreen.tsx
@@ -1,11 +1,18 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
 
-export const UserScreen = () => {
+export const UserScreen = (props: any) => {
+  const {navigation} = props
+
+  const openDrawerHandler = () => {
+    navigation.toggleDrawer()
+  }
+
   return (
     <View style={styles.rootContainer}>
       <Text>
         This is the <Text style={styles.highlight}>"User"</Text> screen!
       </Text>
+      <Button title="Open Drawer!" onPress={openDrawerHandler}/>
     </View>
   );
 }
